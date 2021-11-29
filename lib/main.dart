0df448710c8b35724c44960e0c3999ba3de9891f
seijiro <modeverv@gmail.com>
2021-11-29 23:04:35 +0900

    2021-11-29 23:04:35

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      const MaterialApp(
-        home: Center(
-          child: Text("日本語です"),
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          backgroundColor: Colors.blueAccent,
+          body: const Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://img07.shop-pro.jp/PA01114/681/PA01114681_sp.jpg?cmsp_timestamp=20211018215615'),
+            ),
+          ),
         ),
       ),
     );
